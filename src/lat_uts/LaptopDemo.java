80a8be534a0b5f5c4a9e278561e47a9247de0b70
alyarusydam@gmail.com <alyarusyda31@gmail.com>
2025-11-07 12:23:41 +0700

    update lat_uts

--- src/lat_uts/LaptopDemo.java
+++ src/lat_uts/LaptopDemo.java
@@ -17,12 +17,8 @@ class Laptop {
 }
 
 public class LaptopDemo {
-
     public static void main(String[] args) {
-        Laptop L1 = new Laptop("ASUS", 8, "Intel i5");
-        Laptop L2 = new Laptop("Acer", 16, "AMD Ryzen 7");
-
-        System.out.println(L1.info());
-        System.out.println(L2.info());
+        System.out.println(new Laptop("ASUS", 8, "Intel i5").info());
+        System.out.println(new Laptop("Acer", 16, "AMD Ryzen 7").info());
     }
 }
